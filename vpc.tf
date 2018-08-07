8f1e407c9d648f79d3c185e5e0a623a6472e9294
Alex Gottschalk <alex.gottschalk@gmail.com>
2018-08-07 15:20:09 -0700

    add data layer, start working on route tables

--- vpc.tf
+++ vpc.tf
@@ -6,10 +6,13 @@ resource "aws_vpc" "this" {
 	}
 }
 
-resource "aws_subnet" "public" {
-	count = 3
+resource "aws_internet_gateway" "public" {
 	vpc_id = "${aws_vpc.this.id}"
-	cidr_block = "${}"
+}
+
+resource "aws_subnet" "public" {
+	count = "${var.public_subnet_count}"
+	vpc_id = "${aws_vpc.this.id}"
 	cidr_block = "${cidrsubnet(format("%s/%s", var.vpc_cidr_ip, var.vpc_cidr_mask),
 									var.public_subnet_mask - var.vpc_cidr_mask, count.index)}"
 	availability_zone = "${format("%s%s", var.region, var.azs[count.index%3])}"
@@ -20,13 +23,40 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-	count = 3
+	count = "${var.private_subnet_count}"
 	vpc_id = "${aws_vpc.this.id}"
-	cidr_block = "${}"
 	cidr_block = "${cidrsubnet(format("%s/%s", var.vpc_cidr_ip, var.vpc_cidr_mask),
-									var.private_subnet_mask - var.vpc_cidr_mask, count.index + aws_subnet.public.count - 1)}"
+									var.private_subnet_mask - var.vpc_cidr_mask,
+									count.index + aws_subnet.public.count - 1)}"
 	availability_zone = "${format("%s%s", var.region, var.azs[count.index%3])}"
 	tags {
 		Name = "${format("%s-%s", "private", var.azs[count.index%3])}"
 	}
+}
+
+resource "aws_subnet" "data" {
+	count = "${var.data_subnet_count}"
+	vpc_id = "${aws_vpc.this.id}"
+	cidr_block = "${cidrsubnet(format("%s/%s", var.vpc_cidr_ip, var.vpc_cidr_mask),
+									var.data_subnet_mask - var.vpc_cidr_mask,
+									count.index + aws_subnet.public.count)}"
+	availability_zone = "${format("%s%s", var.region, var.azs[count.index%3])}"
+	map_public_ip_on_launch = false
+	tags {
+		Name = "${format("%s-%s", "data", var.azs[count.index%3])}"
+	}
+}
+
+resource "aws_route_table" "public" {
+	vpc_id = "${aws_vpc.this.id}"
+	route {
+		cidr_block = "0.0.0.0/0"
+		gateway_id = "${aws_internet_gateway.internet.id}"
+	}
+}
+
+resource "aws_route_table_association" "public" {
+	count = "${var.public_subnet_count}"
+	subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
+	route_table_id = "${aws_route_table.public.id}"
 }
