77a94b0374ce0088a72b185119fe68c9b9cba801
Alex Gottschalk <alex.gottschalk@gmail.com>
2018-08-08 11:59:54 -0700

    add security group for VPN

--- openvpnas.tf
+++ openvpnas.tf
@@ -20,9 +20,46 @@ resource "aws_instance" "openvpnas" {
 	user_data = "file://userdata_openvpnas.sh"
 	key_name = "infra"
 	subnet_id = "${random_shuffle.openvpnas_subnet.result.0}"
+	vpc_security_group_ids = ["${aws_security_group.openvpnas.id}", "${aws_vpc.this.default_security_group_id}"]
 }
 
 resource "random_shuffle" "openvpnas_subnet" {
 	input = ["${aws_subnet.public.*.id}"]
 	result_count = 1
+}
+
+resource "aws_security_group" "openvpnas" {
+	name = "openvpn_as"
+	description = "OpenVPN Access Server"
+	vpc_id = "${aws_vpc.this.id}"
+	ingress {
+		from_port = 22
+		to_port = 22
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+	ingress {
+		from_port = 443
+		to_port = 443
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+	ingress {
+		from_port = 943
+		to_port = 943
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+	ingress {
+		from_port = 1194
+		to_port = 1194
+		protocol = "udp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
 }
