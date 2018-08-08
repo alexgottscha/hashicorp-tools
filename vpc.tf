28ceade4b952b7a9d641566eb0911997984d01a9
Alex Gottschalk <alex.gottschalk@gmail.com>
2018-08-08 10:31:30 -0700

    use correct param for NAT gateway

--- vpc.tf
+++ vpc.tf
@@ -66,7 +66,7 @@ resource "aws_route_table" "private" {
 	vpc_id = "${aws_vpc.this.id}"
 	route {
 		cidr_block = "0.0.0.0/0"
-		gateway_id = "${aws_nat_gateway.private.id}"
+		nat_gateway_id = "${aws_nat_gateway.private.id}"
 	}
 }
 
